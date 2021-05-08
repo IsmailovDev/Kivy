0618e6e5481b111d98536bc6bdaf77b666b0cc16
IsmailovDev <diyorbekismailovdev@gmail.com>
2021-05-08 21:17:31 +0300

    uo

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import kivy 
+﻿import kivy 
 import os
 os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 kivy.require('1.9.1') # replace with your current kivy version !
-# sinov uchun
+
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy import Config
